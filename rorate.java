6832529d0626e351688d10b85946225cc491aa2b
Finallysay <58000857+Finallysay@users.noreply.github.com>
2019-11-21 21:02:44 +0800

    Add files via upload

---
+++ rorate.java
@@ -0,0 +1,21 @@
+package parts;
+
+public class rorate {
+    public void rotateArray(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+
+    }
+}
